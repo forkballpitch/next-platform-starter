d78d2f088edcf1f983dc9c6a9136f4799cc661cb
yuri <yuri@yuriui-MacBookPro.local>
2025-06-15 12:45:14 +0900

    commit

--- app/page.jsx
+++ app/page.jsx
@@ -10,7 +10,7 @@ function App() {
     // ncpClientId에 네이버 지도 API 클라이언트 키를 넣으면 된다.
     // npx create-react-app으로 프로젝트를 생성했다면-별도의 의존성 설치 없이-프로젝트 최상위 폴더에 .env 파일을 생성하고 키를 기입하면 된다.
     // .env에는 REACT_APP_NAVER_KEY의 값으로 키를 기입하면 되는데, REACT_APP_라는 prefix에 유의 하자!
-    const naverKey = '6ndkoK6lKyjAvS4TfsYr7afXlEtObhdlw6LZocXX';
+    const naverKey = 'u7amr5n722';
 
     return (
         <NavermapsProvider
